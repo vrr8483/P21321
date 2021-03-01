2f41624f589d8d664d7a5f45058d081bfe1e16e3
James Snedecor <jimbosned@gmail.com>
2021-03-01 18:46:18 -0500

    Update ROV code for readability

--- ROV_Code/Pi/ROV.cpp
+++ ROV_Code/Pi/ROV.cpp
@@ -1,27 +1,25 @@
 #include <cstdio>
 #include <ctime>
+#include <ctime>
+#include <csignal>
+
 #include <chrono>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
 #include <errno.h>
-
 #include <iomanip>
-#include <ctime>
-
 #include <fcntl.h>
 #include <termios.h>
+
 //#include <asm/termbits.h>
 #include <asm/ioctls.h>
 #include <sys/ioctl.h>
 
-#include "SBUS.h"
-
-#include <csignal>
-
 #include <wiringPi.h>
 
+#include "SBUS.h"
 #include "PCA9685.h"
 
 using namespace std::chrono;
@@ -37,13 +35,22 @@ using namespace std::chrono;
 #define SA_DOWN_CHANNEL (5)
 #define SD_ON_CHANNEL (6)
 
+//sensor commands are the packets that the raspberry pi
+//sends to the arduino to tell it to chenge its internally stored sensor value
+//that the receiver polls for. the headers and footers were chosen for
+//their ability to be converted to ASCII. Long story.
 #define SENSOR_CMD_HEADER (0x53)
 #define SENSOR_CMD_FOOTER (0x45)
 
+//The Raspberry pi has two pin addressing modes: Wiring pi (WPI), and Broadcom (BCM).
+//BCM is usually better to use.
 #define INA_WPI_PIN (3)
 #define ENA_WPI_PIN (4)
 #define ENB_WPI_PIN (5)
 #define INB_WPI_PIN (6)
+
+//EN A and EN B are the H bridge enables for path A and B (active high)
+//IN A and IN B are turned on one at a time to indicate the direction of the motor
 
 #define INA_BCM_PIN (22)
 #define ENA_BCM_PIN (23)
@@ -80,8 +87,6 @@ union sensor_cmd_packet_type {
 
 SBUS sbus;
 
-uint32_t value = 0;
-
 PCA9685 pca{};
 
 int arduino_serial_fd;
@@ -91,6 +96,8 @@ std::fstream log_file;
 
 void arduino_serial_init(){
 	//https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/#overview
+    //This will be the only arduino plugged into the Pi,
+    //which is an assumption, but an accurate one.
 	arduino_serial_fd = open("/dev/ttyACM0", O_RDWR);
 	if (arduino_serial_fd < 0){
 		fprintf(stderr, "Arduino serial port open error %i\n", errno);
@@ -170,8 +177,8 @@ void log_data(int t, int dist, int currsense){
 	log_file << line;
 }
 
-//exit fxn
-void signalHandler( int signum ) {
+//called when SIGINT is received (Ctrl+C)
+void signalHandler(int signum) {
    
 	pca.set_pwm(0, 0, 0);
 	pca.set_pwm(1, 0, 0);
@@ -197,8 +204,8 @@ int send_sensor_cmd(uint16_t id, uint32_t new_val){
 	//this stores the packet in big-endian format. 
 	//Raspbian is little-endian, as is Arduino. 
 	//Use endianness_reverse = true (line 6 in 
-	////frsky_arduino.ino as of the time of writing) 
-	////to reverse the endianness of the packet inside arduino.
+	//frsky_arduino.ino as of the time of writing)
+	//to reverse the endianness of the packet inside arduino.
 	/*uint8_t packet_bytes[8];
 	packet_bytes[0] = cmd.header;
 	packet_bytes[1] = cmd.sensor_id >> 8;   
@@ -209,39 +216,39 @@ int send_sensor_cmd(uint16_t id, uint32_t new_val){
 	packet_bytes[6] = cmd.value & 0xFF;
 	packet_bytes[7] = cmd.footer;*/
     	
-    	sensor_cmd_packet_type sensor_packet;
-    	sensor_packet.cmd = cmd;
-    	//debugging printouts
-    	/*for (uint8_t i = 0; i < 8; ++i){
-		printf("%d: %x\t", i, sensor_packet.bytes[i]);
-    	}
-    	printf("\n");
-    	printf("Header: %x\t", sensor_packet.cmd.header);
-    	printf("ID: %x\t", sensor_packet.cmd.sensor_id);
-    	printf("Val: %x\t", sensor_packet.cmd.value);
-    	printf("Footer: %x\t", sensor_packet.cmd.footer);
-    	printf("\n");*/
+    sensor_cmd_packet_type sensor_packet;
+    sensor_packet.cmd = cmd;
+    //debugging printouts
+    /*for (uint8_t i = 0; i < 8; ++i){
+        printf("%d: %x\t", i, sensor_packet.bytes[i]);
+     }
+    printf("\n");
+    printf("Header: %x\t", sensor_packet.cmd.header);
+    printf("ID: %x\t", sensor_packet.cmd.sensor_id);
+    printf("Val: %x\t", sensor_packet.cmd.value);
+    printf("Footer: %x\t", sensor_packet.cmd.footer);
+    printf("\n");*/
 
 	int packet_size = sizeof(sensor_cmd_packet_type);
     	
-    	if (write(sbus._fd, sensor_packet.bytes, packet_size) != packet_size){
-        	fprintf(stderr, "Failed to send sensor command.\n");
+    if (write(sbus._fd, sensor_packet.bytes, packet_size) != packet_size){
+        fprintf(stderr, "Failed to send sensor command.\n");
 		return -1;
-    	}
+    }
 	return 0;
 }
 
 void onPacket(sbus_packet_t packet){
 
 	//printf("Callback called\n");
-    	//printf("now: %ld, lastPrint: %ld\n", now, lastPrint);
-	milliseconds period = milliseconds(100);
-	static milliseconds lastms = duration_cast<milliseconds>(
+    //printf("now: %ld, lastPrint: %ld\n", now, lastPrint);
+	//milliseconds period = milliseconds(100);
+	/*static milliseconds lastms = duration_cast<milliseconds>(
 			system_clock::now().time_since_epoch()
-			);
-	milliseconds ms = duration_cast<milliseconds>(
+			);*/
+	/*milliseconds ms = duration_cast<milliseconds>(
 			system_clock::now().time_since_epoch()
-			);
+			);*/
 
 	/*if (ms - lastms > period){
     		lastms = ms;
@@ -256,6 +263,7 @@ void onPacket(sbus_packet_t packet){
 			packet.failsafe ? "active" : "inactive"
 		);
 		printf("\n");
+        static int value = 0;
 		send_sensor_cmd(0x5958, value++);
 		send_sensor_cmd(0x5900, value/10);
     	}*/
@@ -358,9 +366,7 @@ int main(int argc, char* argv[])
 
 	arduino_serial_init();
 
-	const int num_usb_ports = 4;
-	const std::string port_base = "/dev/ttyUSB";
-
+    
 	std::time_t t = std::time(nullptr);
     	std::tm tm = *std::localtime(&t);
 
@@ -368,6 +374,7 @@ int main(int argc, char* argv[])
 	std::string log_file_prefix = "test_";
 
 	std::string log_file_suffix = "";
+    
 	char timestring_buffer[256];
 	size_t chars_written = std::strftime(timestring_buffer, 256, "_%m_%d_%Y__%H_%M_%S", &tm);
 	if (chars_written == 0){
@@ -379,8 +386,12 @@ int main(int argc, char* argv[])
 	
 	std::string log_filename = log_file_path + log_file_prefix + log_file_suffix;
 
+    
 	//https://www.geeksforgeeks.org/getopt-function-in-c-to-parse-command-line-arguments/
 	int opt;
+    
+    const int num_usb_ports = 4;
+    const std::string port_base = "/dev/ttyUSB";
 	std::string arduino_filepath = port_base + std::to_string(0);
 
 	while ((opt = getopt(argc, argv, ":p:n:")) != -1){
@@ -404,28 +415,31 @@ int main(int argc, char* argv[])
 
 	log_file_init(log_filename);
 
+    //this call passes the onPacket fxn pointer to the SBUS
+    //library for sucessful packet callbacks
     sbus.onPacket(onPacket);
 
     sbus_err_t err = sbus.install(arduino_filepath.c_str(), true);
     
     if (err == SBUS_ERR_OPEN){
-	//loop through /dev/ttyUSB<n> and attempt to find a working USB port
+        //loop through /dev/ttyUSB<n> and attempt to find a working USB port
  
-	printf("Could not open device at %s.\n"
-		"Attempting to find device by searching devices that match %s<n> (searching indices from 0 to %d)...\n", 
-		arduino_filepath.c_str(), port_base.c_str(), num_usb_ports - 1);
+        printf("Could not open device at %s.\n"
+               "Attempting to find device by searching devices that match %s<n> "
+               "(searching indices from 0 to %d)...\n",
+               arduino_filepath.c_str(), port_base.c_str(), num_usb_ports - 1);
 
-	for (int i = 0; i < num_usb_ports; ++i){
-		std::string port_i = port_base + std::to_string(i);
-		err = sbus.install(port_i.c_str(), true);
-		if (err == SBUS_OK){
-			printf("Found device on %s\n", port_i.c_str());
-			break;
-		}
-	}
-	if (err != SBUS_OK){
-		fprintf(stderr, "Could not find device.\n");
-	}
+        for (int i = 0; i < num_usb_ports; ++i){
+            std::string port_i = port_base + std::to_string(i);
+            err = sbus.install(port_i.c_str(), true);
+            if (err == SBUS_OK){
+                printf("Found device on %s\n", port_i.c_str());
+                break;
+            }
+        }
+        if (err != SBUS_OK){
+            fprintf(stderr, "Could not find device.\n");
+        }
     } 
     
     if (err != SBUS_OK) {
